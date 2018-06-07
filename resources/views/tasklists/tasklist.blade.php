63e1fc8c8fc2e4a94d3b537b6b781a598557d4e7
kotaro.kawashiki <kotaro.kawashiki@rakuten.com>
2018-06-07 01:11:33 +0000

    gravatar and composer

--- resources/views/tasklists/tasklist.blade.php
+++ resources/views/tasklists/tasklist.blade.php
@@ -3,6 +3,7 @@
     <?php $user = $tasklist->user; ?>
     <li class="media">
         <div class="media-left">
+            <img class="media-object img-rounded" src="{{ Gravatar::src($user->email, 50) }}" alt="">
         </div>
         <div class="media-body">
             <div>
